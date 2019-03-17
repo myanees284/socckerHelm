1ab8468a2d451aeaef4508231109fb4354cf26f4
myanees284 <44027805+myanees284@users.noreply.github.com>
2019-03-17 15:44:25 +0400

    Update Run_SockkerHelm.sh

--- sockerapp/Run_SockkerHelm.sh
+++ sockerapp/Run_SockkerHelm.sh
@@ -11,19 +11,19 @@ DPASS=${dpass}
 docker login --username $DLOGIN --password $DPASS
 docker build -t $imagever .
 docker push $imagever
-#ibmcloud login --apikey $APIKEY -r eu-de
-#ibmcloud ks cluster-config mycluster
-#export $KUBECONFIG
+ibmcloud login --apikey $APIKEY -r eu-de
+ibmcloud ks cluster-config mycluster
+export $KUBECONFIG
 ##export DEPNAME=$(kubectl get deployment -o json | jq '.items[0].metadata.name')
-#export DEPNAME=$(kubectl get deployment --selector=$sel --output=jsonpath={.items..metadata.name})
-#echo $DEPNAME
-#if [[ $DEPNAME == *"helmsample"* ]]
-#then
- # echo "Updating container IMAGE"
-  #helm package test1
-  #helm upgrade helmsample test1-0.1.2.tgz --set=image.tag=$tagVal
-#else
- # echo "Creating HELM PACKAGE"
-  #helm package test1
-  #helm install test1-0.1.2.tgz --name=helmsample
-#fi
+export DEPNAME=$(kubectl get deployment --selector=$sel --output=jsonpath={.items..metadata.name})
+echo $DEPNAME
+if [[ $DEPNAME == *"you"* ]]
+then
+  echo "Updating container IMAGE"
+  helm package helmapp1
+  helm upgrade helmapp1 helmapp1-0.1.0.tgz --set=image.tag=$tagVal
+else
+  echo "Creating HELM PACKAGE"
+  helm package helmapp1
+  helm install helmapp1-0.1.0.tgz --name=you
+fi
